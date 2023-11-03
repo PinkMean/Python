1f8ea586321e1e95ab8ee2c0cc83018905b3ca58
PinkMean <pinkmeanphotography@gmail.com>
2023-11-03 18:41:34 +0200

    commit

---
+++ Arithmetic.py
@@ -0,0 +1,20 @@
+# Arithmetic Operations
+
+print(1 + 1) # Addition (+)
+print(2 - 1) # Subtraction (-)
+print(2 * 2) # Multiplication (*)
+print(10 - 2) #  Division (/)
+print(3 ** 2)  # Exponential (**)
+print(3 % 2)   # Modulus (%)
+print(3 // 2)  # Floor division operator (//)
+
+# Arithmetic Operations with Label 
+
+print("Addition", 1 + 1)
+print("Subtraction", 2 - 1)
+print("Multiplication", 2 * 2)
+print("Division", 10 - 2)
+
+
+
+
